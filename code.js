ef675f1b171ee2ad26186ebb19b67bf72be0b2ec
MuddathirDawood <dmuddathir@gmail.com>
2022-07-05 11:21:52 +0200

    attribute binding

--- code.js
+++ code.js
@@ -2,10 +2,11 @@
 const app = Vue.createApp({
     data(){
         return{
+            url: 'http://www.thenetninja.co.uk',
             Books:[
-                {title:'Name of The Wind', author: 'Patrick Rothfuss'},
-                {title:'The Way of the Kings', author: 'Brandon Sanderson'},
-                {title:'The Final Empire', author: 'Brandon Sanderson'}
+                {title:'Name of The Wind', author: 'Patrick Rothfuss', img: 'assets/1.jpg'},
+                {title:'The Way of the Kings', author: 'Brandon Sanderson', img: 'assets/2.jpg'},
+                {title:'The Final Empire', author: 'Brandon Sanderson', img: 'assets/3.jpg'}
 
             ],
             showBooks: true,
