abdf876df3f2a2b467fe6e725731e7bd7f977822
MuddathirDawood <dmuddathir@gmail.com>
2022-07-05 11:35:33 +0200

    dynamic classes

--- code.js
+++ code.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
         return{
             url: 'http://www.thenetninja.co.uk',
             Books:[
-                {title:'Name of The Wind', author: 'Patrick Rothfuss', img: 'assets/1.jpg'},
-                {title:'The Way of the Kings', author: 'Brandon Sanderson', img: 'assets/2.jpg'},
-                {title:'The Final Empire', author: 'Brandon Sanderson', img: 'assets/3.jpg'}
+                {title:'Name of The Wind', author: 'Patrick Rothfuss', img: 'assets/1.jpg', isFav: true},
+                {title:'The Way of the Kings', author: 'Brandon Sanderson', img: 'assets/2.jpg', isFav: false},
+                {title:'The Final Empire', author: 'Brandon Sanderson', img: 'assets/3.jpg', isFav: true}
 
             ],
             showBooks: true,
@@ -15,16 +15,6 @@ const app = Vue.createApp({
     methods:{
         toggleBooks(){
             this.showBooks = !this.showBooks
-        },
-        handleEvent(e, data){
-            console.log(e, e.type)
-            if (data) {
-                console.log(data)
-            }
-        },
-        handleMousemove(e){
-            this.x = e.offsetX
-            this.y = e.offsetY
         }
     }
 })
